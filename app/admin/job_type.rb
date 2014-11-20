8eea85f0ebcd4bcc613d303786ffc860a15d766f
John Marbach <john.marbach@gmail.com>
2014-11-20 15:08:26 -0500

    migrated the job type model

---
+++ app/admin/job_type.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register JobType do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+permit_params :stringTopic, :question_id
+
+
+index do
+  selectable_column
+  column 'Job Type', :stringJobType
+  actions
+end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120194737) do
+ActiveRecord::Schema.define(version: 20141120200112) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(version: 20141120194737) do
     t.string   "stringName"
     t.string   "stringLocation"
     t.string   "stringIndustry"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "job_types", force: true do |t|
+    t.string   "stringJobTypec"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
