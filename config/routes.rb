bf8d0b225ded18aa9d61fe50c1cab775b94b5284
John Marbach <john.marbach@gmail.com>
2014-12-10 11:47:50 -0500

    one more change for rich

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount Rich::Engine => '/rich', :as => 'rich'
+ 
+ # mount Rich::Engine => '/rich', :as => 'rich'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
